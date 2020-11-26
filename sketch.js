91397d9af6bb463d349aa60ce98ea235e6f66029
Shaan <you@example.com>
2020-11-26 20:27:09 +0530

    log added

--- sketch.js
+++ sketch.js
@@ -23,7 +23,7 @@ function setup(){
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
     log1 = new Log(810,260,300, PI/2);
-
+    log6=  new Log(500,280,50, PI/2);
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
     pig3 = new Pig(810, 220);
@@ -58,7 +58,7 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
-
+    log6.display();
     bird.display();
     platform.display();
 }
